f2db5863321d3808dfe68f48330048666ce3ed1a
Fahad Sajid <fahadsajid492@gmail.com>
2023-08-23 12:24:42 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Dictaphone from "./Dictaphone";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Speech Recognition</h1>
+      <Dictaphone />
+    </div>
+  );
+}
