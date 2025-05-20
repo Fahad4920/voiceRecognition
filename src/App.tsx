cf2a78f68e981e70c7d767ab1aa29c87804f75c4
Fahad Sajid <124948643+Fahad4920@users.noreply.github.com>
2025-05-20 09:33:22 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Dictaphone from "./Dictaphone";
 export default function App() {
   return (
     <div className="App">
-      <h1>Speech Recognition</h1>
+      <h1>Voice-Speech Recognition</h1>
       <Dictaphone />
     </div>
   );
